ffc980c77a6776cad6d489c232939877cee97012
MisterConscio <saulo.costam@gmail.com>
2023-03-07 12:06:10 -0300

    Adicionado contador do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('Ola')
+const eventDate = new Date("Dec 12, 2023 19:00:00");
+const eventTimeStamp = eventDate.getTime();
+
+const mainInterval = setInterval(function() {
+  const now = new Date();
+  const todayTimeStamp = now.getTime();
+
+  const timeLeft = eventTimeStamp - todayTimeStamp;
+  const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  const timeDisplay = document.getElementById('contador')
+
+  timeDisplay.innerHTML = `${daysLeft}d ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`;
+
+  if (timeLeft < 0) {
+    clearInterval(mainInterval);
+    timeDisplay.innerHTML = "12 de Dezembro de 2023";
+  }
+}, 1000)
